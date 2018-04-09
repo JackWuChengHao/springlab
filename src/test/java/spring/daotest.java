b3e405cec4c764d8343e26cf8f1b793380645908
小吴 <@DESKTOP-F3R2CJ6>
2018-04-09 21:13:23 +0800

    添加跨域REST的支持

--- src/test/java/spring/daotest.java
+++ src/test/java/spring/daotest.java
@@ -5,12 +5,17 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.BeanFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.support.DefaultListableBeanFactory;
+import org.springframework.context.ApplicationContext;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.lab.entity.test;
 
+import java.util.List;
+
+import org.hibernate.Query;
 import org.hibernate.Session;
 
 
@@ -22,20 +27,74 @@ public class daotest {
 	@Autowired 
 	private BeanFactory bf;
 	
+	@Autowired
+	private ApplicationContext ac;
+	
+
+	
 	@Test
-	public void testDataFactory() {
+	public void testGetCount() {
 		
 		SessionFactory sf = (SessionFactory)bf.getBean(SessionFactory.class);
+		System.out.println(bf.getClass().getName());
 		System.out.println(sf);
 		Session se = sf.openSession();
-		test t = new test();
-		t.setNumber(444);
-		Integer a = (Integer) se.save(t);
+		
+		String hql = "select count(*)  from test ";
+		
+		Query query = se.createQuery(hql);
+		
+		Long a = (Long)query.iterate().next();
+		
 		System.out.println(a);
+//		query.setFirstResult(0);
+//		query.setMaxResults(8);
+		
+//		List<test> list = query.list();
+		
+//		for(test i : list) {
+//			System.out.println(i.getNumber());
+//		}
+		
 		se.close();
 		SessionFactory sfsf = (SessionFactory)bf.getBean(SessionFactory.class);
 		System.out.println(sfsf);
 		SessionFactory sfsfsf = (SessionFactory)bf.getBean(SessionFactory.class);
 		System.out.println(sfsfsf);
+	}
+	
+	@Test
+	public void testGetPageList() {
+		SessionFactory sf = (SessionFactory)bf.getBean(SessionFactory.class);
+		System.out.println(sf);
+		Session se = sf.openSession();
+		
+		String hql = "from test ";
+		
+		Query query = se.createQuery(hql);
+		
+		
+		query.setFirstResult(0);
+		query.setMaxResults(8);
+		@SuppressWarnings("unchecked")
+		List<test> list = query.list();
+		
+		for(test i : list) {
+			System.out.println(i.getNumber());
+		}
+		
+		se.close();
+		SessionFactory sfsf = (SessionFactory)bf.getBean(SessionFactory.class);
+		System.out.println(sfsf);
+		SessionFactory sfsfsf = (SessionFactory)bf.getBean(SessionFactory.class);
+		System.out.println(sfsfsf);
+		
+	}
+	
+	
+	@Test
+	public void testBeanFactory() {
+		System.out.println(bf.getClass().getName());
+		DefaultListableBeanFactory a = new DefaultListableBeanFactory();
 	}
 }
